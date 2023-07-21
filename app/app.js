edffefea1f9e3da2387f3286d9f8bf7fec06b082
gogo1414 <wnstn6945@gmail.com>
2023-07-21 23:15:29 +0900

    7/21

--- app/app.js
+++ app/app.js
@@ -1,15 +1,21 @@
+"use strict";
+
+// 모듈
 const express = require('express')
-var cors = require('cors')
 const app = express()
+var cors = require('cors')
 const port = 3000
 
 app.use(cors())
 app.use(express.static('public'))
 
+// 앱세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
+// 라우팅
 const home = require("./src/routes/home");
-app.use("/", home);
+app.use("/", home); // use 미들 웨어 등록 메소드
 
 module.exports = app;
