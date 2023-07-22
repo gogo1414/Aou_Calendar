5520041c53a92695cd69f6ea103b090e6cac02ac
gogo1414 <wnstn6945@gmail.com>
2023-07-22 22:01:20 +0900

    로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("./../../models/User")
 
 const output = {
     login: (req, res) => {
@@ -18,23 +18,8 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const psword = req.body.psword;
-        
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
