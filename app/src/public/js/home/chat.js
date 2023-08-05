51c564b6ca075b317933fe4fb958d9a60daff898
gogo1414 <wnstn6945@gmail.com>
2023-08-05 14:18:38 +0900

    views 배경화면 변경 및 로그아웃 위치 수정 calendar js와 chat js 분리 적용

--- app/src/public/js/home/chat.js
+++ app/src/public/js/home/chat.js
@@ -13,7 +13,7 @@ async function fetchAIResponse(prompt) {
       // API 요청의 헤더를 설정
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer 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`
+        'Authorization': ``
     },
       body: JSON.stringify({
           model: "gpt-3.5-turbo",  // 사용할 AI 모델
